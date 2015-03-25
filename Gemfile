42256f5adb73c10a56e654c943d89b83b9766c88
Cody Gibbs <cgibbs@broco.com>
2015-03-25 01:30:47 -0500

    add resizing

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'mini_magick'
--- giffit.rb
+++ giffit.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
+require 'mini_magick'
 
 get '/' do
 		'Hello, yuns.'
+end
+
+get '/gif/*' do |gif_name|
+		#http://gph.to/1HCPQwE
+		url = "http://media.giphy.com/media/#{gif_name}/giphy.gif"
+
+		size = Hash.new
+		size[:width] = params[:width] || 50
+		size[:height] = params[:height] || 50
+
+		"<img src='#{transform url, size}'>"
+end
+
+get '/*.gif' do |gif_name|
+		File.read("#{gif_name}.gif")
+end
+
+def transform gif_url, size = {}
+		img = MiniMagick::Image.open gif_url
+		img.coalesce
+		img.resize "#{size[:width] || 50}x#{size[:height] || 50}"
+#		img.layers 'Optimize'
+		img.write "./min.gif"
+
+		"/min.gif"
 end
