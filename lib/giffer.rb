a79f32855accff9cb0dad0ae6008876db6e65ae1
Cody Gibbs <cgibbs@broco.com>
2015-03-29 18:04:59 -0500

    Refactor

--- lib/giffer.rb
+++ lib/giffer.rb
@@ -6,17 +6,14 @@ class Giffer
 
     copy = MiniMagick::Image.open gif_url
 
-    gif = MiniMagick::Tool::Convert.new do |img|
-      img << copy.path
+    copy.combine_options do |img|
       img.coalesce
       img.gravity 'center'
       img.resize "#{size[:width] || 50}x#{size[:height] || 50}^"
       img.crop "#{size[:width] || 50}x#{size[:height] || 50}+0+0"
       img.repage.+
-      img << destination
-    end
-
-    MiniMagick::Image.open(destination).to_blob
+      # img << destination
+    end.to_blob
 
   end
 
